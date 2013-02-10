f84931ba0f35981589af15b6a836842dbca2deec
Rotem Rubnov <rotem.rubnov@100grams.nl>
2013-02-10 17:25:56 +0100

    Added missing MBAlertViewItemTypeCustom to MBAlertViewIte.h

--- MBAlertView/Protected/MBAlertViewItem.h
+++ MBAlertView/Protected/MBAlertViewItem.h
@@ -12,11 +12,13 @@ typedef enum {
     MBAlertViewItemTypeDefault,
     MBAlertViewItemTypeDestructive,
     MBAlertViewItemTypePositive,
+    MBAlertViewItemTypeCustom           // if you use this you must set customButton
 }MBAlertViewItemType;
 
 @interface MBAlertViewItem : NSObject
 @property (nonatomic, copy) id block;
 @property (nonatomic, copy) NSString *title;
 @property (nonatomic, assign) MBAlertViewItemType type;
+@property (nonatomic, strong) UIButton *customButton;
 -(id)initWithTitle:(NSString*)text type:(MBAlertViewItemType)type block:(id)block;
 @end
